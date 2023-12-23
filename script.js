70d0e9f4f08f10a844d4fe4e447b07d2d4c0da29
ikagogava <gogavairakli@gmail.com>
2023-12-23 21:29:13 +0400

    1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("phone-menu").style.top = "0";
+  } else {
+    document.getElementById("phone-menu").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
